52c804093bc2dfb57cee82dc605d892bd343867f
Jack Liell-Cock <jackliellcock@gmail.com>
2023-08-26 18:26:28 +0100

    Added details and friends page and made details persist to home page

---
+++ lib/details.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class DetailsPage extends StatefulWidget {
+    final List<(String, String)> details;
+    final Function setDetails;
+    DetailsPage(this.details, this.setDetails);
+
+    @override
+    State<DetailsPage> createState() => DetailsPageState();
+}
+
+class DetailsPageState extends State<DetailsPage> {
+    String ftext = "";
+
+    void dialog(int i) {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) => new AlertDialog(
+                title: new Text(widget.details[i].$1),
+                content: new TextField(
+                    onChanged: (String s) { ftext = s; }
+                ),
+                actions: <Widget>[
+                    new IconButton(
+                        icon: new Icon(Icons.done),
+                        onPressed: (){
+                            widget.setDetails(i, widget.details[i].$1, ftext);
+                            setState((){ });
+                            Navigator.pop(context);
+                        }
+                    ),
+                    new IconButton(
+                        icon: new Icon(Icons.close),
+                        onPressed: (){ Navigator.pop(context); }
+                    ),
+                ]
+            )
+        );
+    }
+
+    @override
+    Widget build(BuildContext context) {
+        return new Scaffold(
+            appBar: new AppBar(
+                backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+                title: new Text("Details")
+            ),
+            body: new ListView.builder(
+                itemCount: widget.details.length,
+                itemBuilder: (BuildContext context, int i) {
+                    return new ListTile(
+                        title: new Text(widget.details[i].$1),
+                        subtitle: new Text(widget.details[i].$2),
+                        onTap: (){ dialog(i); }
+                    );
+                }
+            )
+        );
+    }
+}
