850e3f2b9d34f6b525edcec74d82b654e11923cc
Jack Liell-Cock <jackliellcock@gmail.com>
2023-08-27 13:49:14 +0100

    First attempt at getting location, can access on startup but not persistantly

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
 
 import 'details.dart';
 import 'friends.dart';
@@ -27,16 +31,61 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-    List<(String, String)> details = [
+    List<(String, String)> mDetails = [
         ("Name", "John Doe"),
+        ("Location", ""),
         ("Email", "john.doe@gmail.com"),
         ("LinkedIn", "@john.doe"),
     ];
+    late StreamSubscription<Position> mPositionStream;
 
     void setMainState() {
         setState((){});
     }
 
+    @override
+    void initState() {
+        super.initState();
+        initPositionStream();
+    }
+
+    void initPositionStream() async {
+        bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+        if (!serviceEnabled) {
+            print('Location services are disabled.');
+            return;
+        }
+
+        LocationPermission permission = await Geolocator.checkPermission();
+        if (permission == LocationPermission.deniedForever) {
+            print( 'Location permissions are permanently denied, we cannot request permissions.');
+            return;
+        }
+        if (permission == LocationPermission.denied) {
+            permission = await Geolocator.requestPermission();
+            if (permission == LocationPermission.denied) {
+                print('Location permissions are denied');
+                return;
+            }
+        }
+
+        final locationSettings = LocationSettings(accuracy: LocationAccuracy.low, distanceFilter: 5000);
+        mPositionStream = Geolocator.getPositionStream(locationSettings: locationSettings).listen(
+            (Position? position) { getLocationFromPosition(position); });
+    }
+
+    Future<void> getLocationFromPosition(Position? position) async {
+        print(position);
+        if (position != null) {
+            List<Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
+            if (placemarks != null && placemarks.isNotEmpty) {
+                Placemark placemark = placemarks.first;
+                print(placemark);
+                String loc = '${placemark.locality}, ${placemark.country}';
+                setState((){ mDetails[1] = (mDetails[1].$1, loc); });
+            }
+        }
+    }
 
     @override
     Widget build(BuildContext context) {
@@ -52,8 +101,8 @@ class HomePageState extends State<HomePage> {
                 child: ListView(
                     children: <Widget>[
                         new UserAccountsDrawerHeader(
-                            accountName: new Text(details[0].$2),
-                            accountEmail: new Text(details[1].$2),
+                            accountName: new Text(mDetails[0].$2),
+                            accountEmail: new Text(mDetails[1].$2),
                             currentAccountPicture: new CircleAvatar(
                                 backgroundColor: Colors.black26,
                                 child: new Text(":)")
@@ -74,7 +123,7 @@ class HomePageState extends State<HomePage> {
                             trailing: new Icon(Icons.list_alt),
                             onTap: () => Navigator.of(context).push(
                                 new MaterialPageRoute(
-                                    builder: (BuildContext context) => new DetailsPage(details, setMainState)
+                                    builder: (BuildContext context) => new DetailsPage(mDetails, setMainState)
                                 )
                             )
                         ),
