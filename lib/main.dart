fd9fa177789e8817c9fb9a6aa155ae543488de9f
Jack Liell-Cock <jackliellcock@gmail.com>
2025-04-02 10:03:57 +0100

    added user capabilities

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'package:geocoding/geocoding.dart' as geocoding;
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:async';
 
 import 'details.dart';
 import 'friends.dart';
 import 'firebase_options.dart';
+import 'login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,25 +29,51 @@ class Application extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const HomePage());
+        home: StreamBuilder(
+            stream: FirebaseAuth.instance.idTokenChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              if (snapshot.data != null) {
+                return HomePage(snapshot.data!);
+              }
+              return const LoginPage();
+            }));
   }
 }
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  final User mUser;
+
+  const HomePage(this.mUser, {super.key});
 
   @override
   State<HomePage> createState() => HomePageState();
 }
 
 class HomePageState extends State<HomePage> {
-  List<(String, String)> mDetails = [
-    ("Name", "John Doe"),
-    ("Location", ""),
-    ("Email", "john.doe@gmail.com"),
-    ("LinkedIn", "@john.doe"),
-  ];
+  HomePageState() {
+    mDetails = [
+      (
+        "Name",
+        FirebaseAuth.instance.currentUser?.displayName == null
+            ? ""
+            : FirebaseAuth.instance.currentUser!.displayName!
+      ),
+      ("Location", ""),
+      (
+        "Email",
+        FirebaseAuth.instance.currentUser?.email == null
+            ? ""
+            : FirebaseAuth.instance.currentUser!.email!
+      ),
+    ];
+  }
+
+  late List<(String, String)> mDetails;
   final Location mLocation = Location();
+  StreamSubscription<LocationData>? mLocationSub;
 
   void setMainState() {
     setState(() {});
@@ -76,7 +105,7 @@ class HomePageState extends State<HomePage> {
       }
     }
 
-    mLocation.onLocationChanged.listen((LocationData location) {
+    mLocationSub = mLocation.onLocationChanged.listen((LocationData location) {
       getLocation(location);
     });
   }
@@ -88,13 +117,13 @@ class HomePageState extends State<HomePage> {
       if (placemarks.isNotEmpty) {
         geocoding.Placemark p = placemarks.first;
         String loc = "";
-        if (p.locality != null && p.locality!.isNotEmpty) {
-          loc += "${p.locality!}, ";
-        } else if (p.subAdministrativeArea != null &&
-            p.subAdministrativeArea!.isNotEmpty)
-          loc += "${p.subAdministrativeArea!}, ";
         if (p.country != null) {
-          loc += p.country!;
+          if (p.locality != null && p.locality!.isNotEmpty) {
+            loc = "${p.locality!}, ${p.country!}";
+          } else if (p.subAdministrativeArea != null &&
+              p.subAdministrativeArea!.isNotEmpty) {
+            loc = "${p.subAdministrativeArea!}, ${p.country!}";
+          }
         }
         setState(() {
           mDetails[1] = (mDetails[1].$1, loc);
@@ -133,6 +162,13 @@ class HomePageState extends State<HomePage> {
             onTap: () => Navigator.of(context).push(MaterialPageRoute(
                 builder: (BuildContext context) =>
                     DetailsPage(mDetails, setMainState)))),
+        ListTile(
+            title: const Text("Log Out"),
+            trailing: const Icon(Icons.exit_to_app),
+            onTap: () {
+              mLocationSub?.cancel();
+              FirebaseAuth.instance.signOut();
+            }),
       ])),
       //floatingActionButton: FloatingActionButton(),
     );
